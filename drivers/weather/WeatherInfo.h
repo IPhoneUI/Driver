96a8d900abc9a03e18718992ecc67cdd8fd248a1
thaivodinh <thaivodinh99@gmail.com>
2025-01-18 22:49:08 +0700

    update

--- drivers/weather/WeatherInfo.h
+++ drivers/weather/WeatherInfo.h
@@ -16,16 +16,17 @@ public:
         return mData;
     }
 
-    void setWeatherData(const std::string& location, const double& currentTmp, const double& maxTemp, const double& minTemp);
-    void fetchData(const std::string& key);
-    service::LocationCoordinate getDestination() const;
-    void parseData();
     std::string getRawData() const {
         return mRawData;
     }
     std::string getNameLocation() const {
         return mData.mLocationName;
     }
+
+    void setWeatherData(const std::string& location, const double& currentTmp, const double& maxTemp, const double& minTemp);
+    void fetchData(const std::string& key);
+    service::LocationCoordinate getDestination() const;
+    void parseData();
 
 private:
     service::WeatherParameters mData;
