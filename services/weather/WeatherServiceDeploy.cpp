ff46341846ab983c50b13fe9d5c7172284b69d3f
thaivodinh <thaivodinh99@gmail.com>
2025-01-18 15:56:09 +0700

    update

--- services/weather/WeatherServiceDeploy.cpp
+++ services/weather/WeatherServiceDeploy.cpp
@@ -28,7 +28,7 @@ void WeatherServiceDeploy::responseWeatherDataChanged()
 {
     mClientManager.deploy([this](std::string mqName) {
         std::lock_guard<std::mutex> lock(mMutex);
-        mMqSender.startMsq(base::msq::Msq_SysSett_RespAirPlaneModeUpdated);
+        mMqSender.startMsq(base::msq::Msq_Weather_RespDataChanged);
         mMqSender.sendMsq(mqName);
     });
 }
